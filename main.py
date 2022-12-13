2e0d1a862997c07cc553d30ecb8f656b6f95c8f3
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2022-12-13 17:24:44 +0100

    Added base
    Still need to work out how to detect if a loop was found or not (4 --> 1 --> 4 --> 1 and so on), please help if you know how.

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+tempNumber = 1
+triedCounter = 1
+gotOneCounter = 1
+
+if __name__ == '__main__':
+    while tempNumber >= 0:
+        tempNumber = int(tempNumber) + 1
+
+        if tempNumber % 2:
+            tempNumber = int(tempNumber) / 2
+            if int(tempNumber) == 1:
+                gotOneCounter += 1
+
+            triedCounter += 1
+        else:
+            tempNumber = int(tempNumber) * 3 + 1
+            if int(tempNumber) == 1:
+                gotOneCounter += 1
+
+            triedCounter += 1
+
+        print("[Brute Force] [3X + 1 System Message] New combination tried.")
+
+        print(f"[Brute Force] [3X + 1 System Message] {triedCounter} Combinations tried so far.")
+        print(f"[Brute Force] [3X + 1 System Message] {gotOneCounter} Loops passed so far.")
